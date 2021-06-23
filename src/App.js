fd19353be828195594d9ee9c4cb981a0aed1afa6
sowmiya seshadri <sowmiya seshadri>
2021-06-23 22:26:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout />
+      </div>
+    );
+  }
+}
+
+export default App;
